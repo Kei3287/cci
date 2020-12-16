9142b615bc754072065aa29abba88e5515b4dcc9
Keisuke Watanabe <kei3287@berkeley.edu>
2020-12-15 21:52:37 -0800

    add URLify

--- array_and_string.py
+++ array_and_string.py
@@ -1,4 +1,4 @@
-# O(1) space for 256 ASCII chars
+# O(1) space for 256 ASCII chars (There's only 128 printable characters, but extended ASCII has 256)
 # O(n) time
 def is_unique(str):
   count = {}
@@ -16,10 +16,22 @@ print(is_unique("avda"))
 def check_permutation(str1, str2):
   if len(str1) != len(str2):
     return False
-  counts = {}
+  counts = [0] * 256
   for i in range(len(str1)):
-    counts[str1[i]] = counts.get(str1[i], 0) + 1
-    counts[str2[i]] = counts.get(str2[i], 0) - 1
-  return all(map(lambda x: x == 0, counts.values()))
+    counts[ord(str1[i])] = counts[ord(str1[i])] + 1
+    counts[ord(str2[i])] = counts[ord(str2[i])] - 1
+  return sum(counts) == 0
 
 print(check_permutation("avva", "aavv"))
+
+
+def URLify(str, size):
+  output = ""
+  for i in range(len(str)):
+    if str[i] == " ":
+      output += "%20"
+    else:
+      output += str[i]
+  return output
+
+print(URLify("aa a", 4))
