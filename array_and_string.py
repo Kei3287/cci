c26734ff4487aabbe0d5414cd5c18288b961d277
Keisuke Watanabe <kei3287@berkeley.edu>
2020-12-16 20:45:34 -0800

    add string compression

--- array_and_string.py
+++ array_and_string.py
@@ -1,8 +1,8 @@
 # O(1) space for 256 ASCII chars (There's only 128 printable characters, but extended ASCII has 256)
 # O(n) time
-def is_unique(str):
+def is_unique(str1):
   count = {}
-  for s in str:
+  for s in str1:
     if s in count:
       return False
     else:
@@ -28,13 +28,13 @@ print("Test 1.2")
 print(check_permutation("avva", "aavv"))
 print()
 
-def URLify(str, size):
+def URLify(str1, size):
   output = ""
-  for i in range(len(str)):
-    if str[i] == " ":
+  for i in range(len(str1)):
+    if str1[i] == " ":
       output += "%20"
     else:
-      output += str[i]
+      output += str1[i]
   return output
 
 print("Test 1.3")
@@ -43,10 +43,10 @@ print()
 
 # O(n) space
 # O(n) time
-def palindrome_permutation(str):
+def palindrome_permutation(str1):
   counts = {}
   odd_count = 0
-  for s in str:
+  for s in str1:
     if s == " ":
       continue
     counts[s] = counts.get(s, 0) + 1
@@ -54,7 +54,7 @@ def palindrome_permutation(str):
       odd_count += 1
     else:
       odd_count -= 1
-  if len(str) % 2 == 0:
+  if len(str1) % 2 == 0:
     return odd_count == 1
   else:
     return odd_count == 0
@@ -94,4 +94,26 @@ print(one_away("avb", "ab"))
 print(one_away("avb", "acb"))
 print(one_away("avb", "avbx"))
 print(one_away("aaa", "avv"))
+print()
+
+# O(n) space
+# O(n) time
+# string concatenation of whole string takes O(n^2) because it copies both strings every time. 1 + 2 + ... + n = n(n+1)/2
+# => reduce the number of concatenation
+def string_compression(str1):
+  output = ""
+  count = 0
+  for i in range(len(str1)):
+    count += 1
+    if i == len(str1) - 1 or str1[i] != str1[i+1]:
+      output = output + str1[i] + str(count)
+      count = 0
+  if len(output) > len(str1):
+    return str1
+  return output
+
+print("Test 1.6")
+print(string_compression("aabbb"))
+print(string_compression("avc"))
+print(string_compression("bbbbc"))
 print()
