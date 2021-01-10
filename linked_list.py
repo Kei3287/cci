a71eecf68cbb6ea6818736b0f13c0b94105ec483
Keisuke Watanabe <kei3287@berkeley.edu>
2021-01-10 12:03:06 -0800

    wip

--- linked_list.py
+++ linked_list.py
@@ -23,78 +23,77 @@ class LinkedList():
             print(ptr.val)
             ptr = ptr.next
 
-    def remove_dups(self):
-        """
-            Remove the duplicate elements.
-            O(N) space
-            O(N) time
-            (O(NlogN) time & O(1) space if we sort the list at the beginning.)
-        """
-        d = set()
-        prev = self.head
-        n = self.head
-        while n != None:
-            if n.val in d:
-                prev.next = n.next
-            else:
-                d.add(n.val)
-                prev = n
-            n = n.next
+def remove_dups(ll):
+    """
+        Remove the duplicate elements.
+        O(N) space
+        O(N) time
+        (O(NlogN) time & O(1) space if we sort the list at the beginning.)
+    """
+    d = set()
+    prev = ll.head
+    n = ll.head
+    while n != None:
+        if n.val in d:
+            prev.next = n.next
+        else:
+            d.add(n.val)
+            prev = n
+        n = n.next
 
-    def kth_to_last(self, k):
-        """
-            Return the kth to last element of a singly linked list.
-            O(1) space
-            O(N) time
-        """
-        p1 = self.head
-        p2 = self.head
-        for _ in range(k):
-            if p2.next is None:
-                return None
-            p2 = p2.next
-        while p2 != None:
-            p1 = p1.next
-            p2 = p2.next
-        return p1.val
+def kth_to_last(ll, k):
+    """
+        Return the kth to last element of a singly linked list.
+        O(1) space
+        O(N) time
+    """
+    p1 = ll.head
+    p2 = ll.head
+    for _ in range(k):
+        if p2.next is None:
+            return None
+        p2 = p2.next
+    while p2 != None:
+        p1 = p1.next
+        p2 = p2.next
+    return p1.val
 
-    def kth_to_last_rec(self, k):
-        """
-            Recursive solution for kth to last problem
-            O(N) space
-            O(N) time
-        """
-        kth_val = 0
-        def kth_to_last_helper(head, k):
-            nonlocal kth_val
-            if head is None:
-                return 0
-            index = kth_to_last_helper(head.next, k) + 1
-            if index == k:
-                kth_val = head.val
-            return index
-        kth_to_last_helper(self.head, k)
-        return kth_val
+def kth_to_last_rec(ll, k):
+    """
+        Recursive solution for kth to last problem
+        O(N) space
+        O(N) time
+    """
+    kth_val = 0
+    def kth_to_last_helper(head, k):
+        nonlocal kth_val
+        if head is None:
+            return 0
+        index = kth_to_last_helper(head.next, k) + 1
+        if index == k:
+            kth_val = head.val
+        return index
+    kth_to_last_helper(ll.head, k)
+    return kth_val
 
-    def partition(self, x):
-        """
-            Partition a linked list around a value x such that all nodes less than x comes before all nodes greater than or equal to x.
-            ex) x = 5
-                input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1
-                output: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8 OR 3 -> 1 -> 2 -> 5 -> 8 -> 5 -> 10, etc (value x can be anywhere as long as all values less than x is before x.)
-            O(1) space
-            O(N) time
-        """
-        ptr = self.head.next
-        prev = self.head
-        while ptr != None:
-            if ptr.val < x:
-                self.head = Node(ptr.val, next = self.head)
-                prev.next = ptr.next
-            else:
-                prev = ptr
-            ptr = ptr.next
-
+def partition(ll, x):
+    """
+        Partition a linked list around a value x such that all nodes less than x comes before all nodes greater than or equal to x.
+        ex) x = 5
+            input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1
+            output: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8 OR 3 -> 1 -> 2 -> 5 -> 8 -> 5 -> 10, etc (value x can be anywhere as long as all values less than x is before x.)
+        O(1) space
+        O(N) time
+    """
+    ptr = ll.head.next
+    prev = ll.head
+    while ptr != None:
+        if ptr.val < x:
+            ll.head = Node(ptr.val, next = ll.head)
+            prev.next = ptr.next
+        else:
+            prev = ptr
+        ptr = ptr.next
 
 ll1 = LinkedList()
 ll1.append(1)
@@ -105,7 +104,7 @@ ll1.append(2)
 ll1.print()
 
 print("Test 2.1")
-ll1.remove_dups()
+remove_dups(ll1)
 print("Should print 1 2 3")
 ll1.print()
 print()
@@ -121,13 +120,13 @@ ll2.print()
 
 print("Test 2.1")
 print("Should print 5")
-print(ll2.kth_to_last(1))
+print(kth_to_last(ll2, 1))
 print("Should print 3")
-print(ll2.kth_to_last(3))
+print(kth_to_last(ll2, 3))
 print("Should print 5")
-print(ll2.kth_to_last_rec(1))
+print(kth_to_last_rec(ll2, 1))
 print("Should print 3")
-print(ll2.kth_to_last_rec(3))
+print(kth_to_last_rec(ll2, 3))
 print()
 
 
@@ -162,7 +161,7 @@ ll4.append(8)
 ll4.append(4)
 ll4.append(10)
 print("Test 2.3")
-ll4.partition(10)
+partition(ll4, 10)
 print("Should print 4 8 1 11 10")
 ll4.print()
 print()
