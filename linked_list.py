f5929661a3a3628eb4aa0084887f8e070a0cf44e
Keisuke Watanabe <kei3287@berkeley.edu>
2021-01-09 22:09:26 -0800

    add 2.1 remove dups

---
+++ linked_list.py
@@ -0,0 +1,49 @@
+class Node():
+    def __init__(self, val = 0, next = None):
+        self.next = next
+        self.val = val
+
+class LinkedList():
+    def __init__(self, head = None):
+        self.head = None
+
+    def append(self, val):
+        end = Node(val)
+        if self.head is None:
+            self.head = end
+            return
+        ptr = self.head
+        while ptr.next != None:
+            ptr = ptr.next
+        ptr.next = end
+
+    def print(self):
+        ptr = self.head
+        while ptr != None:
+            print(ptr.val)
+            ptr = ptr.next
+
+    def remove_dups(self):
+        d = set()
+        prev = self.head
+        n = self.head
+        while n != None:
+            if n.val in d:
+                prev.next = n.next
+            else:
+                d.add(n.val)
+                prev = n
+            n = n.next
+
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(3)
+ll.append(2)
+ll.print()
+
+print("Test 2.1")
+ll.remove_dups()
+# Should print 1 2 3
+ll.print()
